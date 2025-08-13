9bcb5c25f64582fd0797876b783cd17a06cdb25b
Chen Liu <chen.liu.cl2482@yale.edu>
2025-08-12 21:29:54 -0400

    update license

--- setup.py
+++ setup.py
@@ -22,9 +22,9 @@ setup(
         ],
     },
     classifiers=[
-    'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
-    'Intended Audience :: Developers',      # Define that your audience are developers
-    'License :: MIT',                       # Again, pick a license
-    'Programming Language :: Python :: 3',  # Specify which pyhton versions that you want to support
+    'Development Status :: 3 - Alpha',         # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
+    'Intended Audience :: Developers',         # Define that your audience are developers
+    'License :: OSI Approved :: MIT License',  # Again, pick a license
+    'Programming Language :: Python :: 3',     # Specify which pyhton versions that you want to support
     ],
 )
